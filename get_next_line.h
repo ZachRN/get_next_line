3198108668878e50430a86c6ae5bb39299f0b546
Zachary Najda <znajda@f0r6s6.codam.nl>
2021-03-11 17:34:28 +0100

    Updating header file to include a define and the libft functions

--- get_next_line.h
+++ get_next_line.h
@@ -9,6 +9,10 @@
 # include <stdlib.h>
 
 # define FD_TOTAL 128
-int	get_next_line(int fd, char **line);
-
+int		get_next_line(int fd, char **line);
+char	*ft_strchr(const char *str, int c);
+int		ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 #endif
