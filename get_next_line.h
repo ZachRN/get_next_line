cf8cdb148cd730ac33dcb51234e26e140ba245d3
Zachary Najda <znajda@f0r6s6.codam.nl>
2021-03-18 18:13:24 +0100

    Changed define BUFFER_SIZE to work without being defined in the compile phase

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,14 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 32;
-# endif
+
 # include <stdio.h>
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
 
 # define FD_TOTAL 128
 int		get_next_line(int fd, char **line);
