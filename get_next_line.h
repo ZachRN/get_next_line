637041bd7136adc55e111f8f5ecdcc1044dd8635
Zachary Najda <znajda@f1r4s6.codam.nl>
2021-05-13 11:55:39 +0200

    Just made it a bit prettier to look at

--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
@@ -11,10 +10,12 @@
 # endif
 
 # define FD_TOTAL 128
+
 int		get_next_line(int fd, char **line);
 char	*ft_strchr(const char *str, int c);
 int		ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
 #endif
