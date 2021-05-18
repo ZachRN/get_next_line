159173b323f4de136e4fab88f1ed155732d86c13
Zachary Najda <znajda@f1r4s3.codam.nl>
2021-05-18 17:48:33 +0200

    Updating to fix memory leak, and freeing upon a failure

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,4 @@
 #include "get_next_line.h"
-#include <stdio.h>
 
 int	send_new_line(char **line, int fd, t_filehold *file, int pos)
 {
@@ -8,15 +7,14 @@ int	send_new_line(char **line, int fd, t_filehold *file, int pos)
 
 	temp = (char *)malloc(sizeof(char) * pos + 1);
 	if (!temp)
+	{
+		free(file->all_fd[fd]);
 		return (-1);
+	}
 	ft_strlcpy(temp, file->all_fd[fd], pos + 1);
-	if (!temp)
-		return (-1);
 	*line = temp;
 	strlen = ft_strlen(file->all_fd[fd]);
 	ft_strlcpy(file->all_fd[fd], &file->all_fd[fd][pos + 1], strlen - pos + 1);
-	if (!file->all_fd[fd])
-		return (-1);
 	return (1);
 }
 
@@ -27,15 +25,14 @@ int	send_EOF(char **line, int fd, t_filehold *file, int pos)
 
 	temp = (char *)malloc(sizeof(char) * pos + 1);
 	if (!temp)
+	{
+		free(file->all_fd[fd]);
 		return (-1);
+	}
 	ft_strlcpy(temp, file->all_fd[fd], pos + 1);
-	if (!temp)
-		return (-1);
 	*line = temp;
 	strlen = ft_strlen(file->all_fd[fd]);
 	ft_strlcpy(file->all_fd[fd], &file->all_fd[fd][pos + 1], strlen - pos + 1);
-	if (!file->all_fd[fd])
-		return (-1);
 	return (0);
 }
 
@@ -60,7 +57,10 @@ int	process_next_line(int fd, t_filehold *files, int readbytes)
 	files->buf[readbytes] = '\0';
 	temp = ft_strjoin(files->all_fd[fd], files->buf);
 	if (!temp)
-		return (-1);
+	{
+		free(files->all_fd[fd]);
+		return (0);
+	}
 	free(files->all_fd[fd]);
 	files->all_fd[fd] = temp;
 	return (1);
@@ -71,13 +71,14 @@ int	get_next_line(int fd, char **line)
 	static t_filehold	files;
 	int					readbytes;
 
-	if (fd == -1 || !line || BUFFER_SIZE < 1)
+	if (fd == -1 || !line || BUFFER_SIZE < 1 || fd > FD_TOTAL)
 		return (-1);
 	if (!files.all_fd[fd])
 		files.all_fd[fd] = ft_strdup("\0");
 	if (!files.all_fd[fd])
 		return (-1);
-	readbytes = read(fd, files.buf, BUFFER_SIZE);
+	readbytes = 1;
+	files.buf[0] = '\0';
 	while (readbytes > 0)
 	{
 		if (!process_next_line(fd, &files, readbytes))
@@ -87,6 +88,9 @@ int	get_next_line(int fd, char **line)
 		readbytes = read(fd, files.buf, BUFFER_SIZE);
 	}
 	if (readbytes == -1)
+	{
+		free(files.all_fd[fd]);
 		return (-1);
+	}
 	return (file_stuff(line, &files, fd));
 }
